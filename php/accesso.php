fa1f3b18aa7639b28a0f7fdea67e20b06a5c0aa5
Lucia Monetta <lucia.monetta4@gmail.com>
2026-01-25 12:52:22 +0100

    m

--- php/accesso.php
+++ php/accesso.php
@@ -68,7 +68,7 @@ if (isset($_POST['register'])) {
 <html lang="it">
 <head>
 <meta charset="UTF-8">
-<meta name="author" cotent="gruppo26"/>
+<meta name="author" content="gruppo26"/>
 <meta name="description" content="Pagina di accesso"/>
 <title>Accesso – Portale Tecnologie Web</title>
 <link href="../css/accesso.css" rel="stylesheet" type="text/css" >
