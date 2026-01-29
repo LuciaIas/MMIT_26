baeb5f036e973b95ced3c6217a9a3a180eaddf41
Lucia Monetta <lucia.monetta4@gmail.com>
2026-01-29 23:31:18 +0100

    m

--- php/db.php
+++ php/db.php
@@ -6,9 +6,5 @@ $dbname = "gruppo26";
 $user = "www";
 $password = "www";
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-if (!$conn) {
-    die("Connessione fallita: " . pg_last_error());
-}
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password") or die('Impossibile connettersi al database: ' . pg_last_error());
 ?>
